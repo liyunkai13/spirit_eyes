a3aa32c42ead904bed93500e77d5302e6b5193e8
liyunkai13 <474769828@qq.com>
2024-03-14 13:47:31 +0800

    引入antd菜单；大概布局

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {SideMenu} from './components/SideMenu';
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/*顶部信息栏*/}
+        <div className="Top-info" style={{margin:"5vh",display:"flex",flexDirection:"row",height: 10,alignItems:"center"}}>
+          <img src={logo} className="App-logo" alt="logo" />
+          <>这是一个测试</>
+        </div>
+
+        {/*主界面部分*/}
+        <div className="Body" style={{width:"100vw",display:"flex",flexDirection:"row",alignItems:"flex-start",background:"yellow"}}>
+          {/*侧边导航栏*/}
+          <SideMenu className="Side-menu" />
+            {/*主界面*/}
+          <div
+              className="Main-content"
+              style={{
+                flex: 1,
+                width: "85vw",
+                height: '100vh',
+                background: 'blue',
+              }}
+            >
+          </div>
+        </div>
+
+
+
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        {/*<p>*/}
+        {/*  Edit <code>src/App.js</code> and save to reload.*/}
+        {/*</p>*/}
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+        {/*  Learn React*/}
+        {/*</a>*/}
       </header>
+
+
+
     </div>
   );
 }
