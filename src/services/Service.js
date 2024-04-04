0791c4ab763e781a42609f6f288f3855cda2afc7
liyunkai13 <474769828@qq.com>
2024-04-04 19:31:15 +0800

    合并下文件

---
+++ src/services/Service.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3001'; // 假设这是你的后端API地址
+
+// const userService = {
+//     async fetchUsers() {
+//         try {
+//             const response = await axios.get(`${BASE_URL}/users`);
+//             return response.data;
+//         } catch (error) {
+//             throw new Error('Failed to fetch users');
+//         }
+//     }
+// };
+const WardsService = {
+    async fetchWards() {
+        try {
+            const response = await axios.get(`${BASE_URL}/wards`);
+            return response.data;
+        } catch (error) {
+            throw new Error('Failed to fetch Wards');
+        }
+    }
+};
+export default WardsService;
+
+
